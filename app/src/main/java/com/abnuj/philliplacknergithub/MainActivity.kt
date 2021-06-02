3124b62d504c062a88702afafb65d21e14ab00ec
AmmbujPanddey <ambuj3224@gmail.com>
2021-06-02 23:11:02 +0530

    third commit

--- app/src/main/java/com/abnuj/philliplacknergithub/MainActivity.kt
+++ app/src/main/java/com/abnuj/philliplacknergithub/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println(" this is container in my second commit")
+        today is very good day we can say it
     }
 }
